b88311ea8d805eabfe6d6167d39da7b56c900a4e
Francis Colendres <flcolendres@myseneca.ca>
2022-01-28 15:52:47 -0500

    Started DIY

---
+++ WS02/DIY/DNA.cpp
@@ -0,0 +1,7 @@
+/**
+* Francis Lloyd Colendres
+* flcolendres@myseneca.ca
+* 145498200
+* Jan. 23, 2022
+* I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
---
+++ WS02/DIY/cStrTools.h
@@ -0,0 +1,26 @@
+/**
+* Francis Lloyd Colendres
+* flcolendres@myseneca.ca
+* 145498200
+* Jan. 28, 2022
+* I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#ifndef SDDS_CSTRTOOLS_H 
+#define SDDS_CSTRTOOLS_H
+namespace sdds {
+
+	char toLower(char ch);
+	int strCmp(const char* s1, const char* s2);
+	int strnCmp(const char* s1, const char* s2, int len);
+	void strCpy(char* des, const char* src);
+	int strLen(const char* str);
+	const char* strStr(const char* str, const char* find);
+	int isAlpha(char ch);
+	int isSpace(char ch);
+	void trim(char word[]);
+	void toLowerCaseAndCopy(char des[], const char source[]);    
+	void read(char* cString, unsigned int maxSize, char delimiter);
+
+}
+#endif
