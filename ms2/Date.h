7ba09079f601ba2e9fa988ed3518dcee224e575d
Francis Colendres <flcolendres@myseneca.ca>
2022-03-12 21:25:22 -0500

    Overwriting Date, Status, and Utils repo

---
+++ ms2/Date.h
@@ -0,0 +1,50 @@
+/* ------------------------------------------------------
+Final project Milestone 1
+Module: Date
+Filename: Date.h
+Version 1.0
+Author: Francis Lloyd Colendres  2022-03-08
+Revision History
+-----------------------------------------------------------
+Date          Reason
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+#ifndef SDDS_DATE_H
+#define SDDS_DATE_H
+#include <iostream>
+#include "Status.h"
+namespace sdds
+{
+   class Date
+   {
+      const int m_maxyear = 2030;
+      int m_year;
+      int m_month;
+      int m_day;
+      Status m_state;
+      bool m_format = true;
+      bool validate();
+      int uniqueDateVal();
+   public:
+      Date(const int year = 0, const int month = 0, const int date = 0);
+      Date& operator=(const Date& d);
+      bool operator==(Date& d);
+      bool operator!=(Date& d);
+      bool operator<(Date& d);
+      bool operator>(Date& d);
+      bool operator<=(Date& d);
+      bool operator>=(Date& d);
+      const Status& state()const;
+      Date& formatted(bool set);
+      operator bool()const;
+      std::ostream& write(std::ostream& ostr) const;
+      std::istream& read(std::istream& istr);
+   };
+   std::ostream& operator<<(std::ostream& ostr, const Date& d);
+   std::istream& operator>>(std::istream& istr, Date& d);
+
+}
+#endif
---
+++ ms2/Status.cpp
@@ -0,0 +1,105 @@
+/* ------------------------------------------------------
+Final project Milestone 1
+Module: Status
+Filename: Status.cpp
+Version 1.0
+Author: Francis Lloyd Colendres  2022-03-08
+Revision History
+-----------------------------------------------------------
+Date          Reason
+03/11         Redefined operator <<
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#include <iostream>
+#include <cstring>
+#include "Status.h"
+#include "Utils.h"
+using namespace std;
+namespace sdds {
+   Status::Status(const char* c)
+   {
+      ut.alocpy(m_errDesc, c);
+      m_errCode = 0;
+   }
+
+   Status::Status(const Status& s)
+   {
+      if (s.m_errDesc != nullptr)
+      {
+         ut.alocpy(m_errDesc, s.m_errDesc);
+         m_errCode = s.m_errCode;
+      }
+   }
+
+   Status& Status::operator=(const Status& s)
+   {
+      if (this != &s)
+      {
+         if (s.m_errDesc != nullptr)
+         {
+            ut.alocpy(m_errDesc, s.m_errDesc);
+            m_errCode = s.m_errCode;
+         }
+      }
+      return *this;
+   }
+
+   Status::~Status()
+   {
+      clear();
+   }
+
+   Status& Status::operator=(int num)
+   {
+      m_errCode = num;
+      return *this;
+   }
+
+   Status& Status::operator=(const char* str)
+   {
+      ut.alocpy(m_errDesc, str);
+      return *this;
+   }
+
+   Status::operator int()const
+   {
+      return m_errCode;
+   }
+
+   Status::operator const char* () const
+   {
+      return m_errDesc;
+   }
+
+   Status::operator bool() const
+   {
+      return m_errDesc == nullptr;
+   }
+
+   Status& Status::clear()
+   {
+      delete[] m_errDesc;
+      m_errDesc = nullptr;
+      m_errCode = 0;
+      return *this;
+   }
+
+   ostream& operator<<(ostream& ostr, const Status& s)
+   {
+      if (!s)
+      {
+         if (int(s) != 0)
+         {
+            ostr << "ERR#" << int(s) << ": " << (const char*)s;
+         }
+         else
+         {
+            ostr << (const char*)s;
+         }
+      }
+      return ostr;
+   }
+
+}
