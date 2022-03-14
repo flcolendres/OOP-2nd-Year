35285de8648ba6f65ae40e50cf93c6033f646f4b
Francis Colendres <flcolendres@myseneca.ca>
2022-03-14 01:51:35 -0400

    added blank header files

---
+++ WS07/lab/Container.cpp
@@ -0,0 +1,11 @@
+/**
+* Francis Lloyd Colendres
+* flcolendres@myseneca.ca
+* 145498200
+* Feb. 27, 2022
+* I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+#include "Container.h"
+namespace sdds {
+
+}
---
+++ WS07/lab/Container.h
@@ -0,0 +1,14 @@
+/**
+* Francis Lloyd Colendres
+* flcolendres@myseneca.ca
+* 145498200
+* Feb. 27, 2022
+* I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+#ifndef SDDS_CONTAINER_H
+#define SDDS_CONTAINER_H
+namespace sdds {
+
+}
+
+#endif
