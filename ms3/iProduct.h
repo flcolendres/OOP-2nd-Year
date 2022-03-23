8212659f172b79da3e12d97a849fd159188711e2
Francis Colendres <flcolendres@myseneca.ca>
2022-03-22 21:47:18 -0400

    turning the prototyes into a pure virtual function

--- ms3/iProduct.h
+++ ms3/iProduct.h
@@ -19,34 +19,34 @@ namespace sdds
 {
    class iProduct
    {
-      int readSku(std::istream& istr);
+      virtual int readSku(std::istream& istr) = 0;
       // to reduce the quantity on hand
-      int operator-=(int qty);
+      virtual int operator-=(int qty) = 0;
       // to increase the quantity on hand
-      int operator+=(int qty);
+      virtual int operator+=(int qty) = 0;
       // returns the price of the produce
-      operator double()const;
+      virtual operator double()const = 0;
       // returns if the iProduct is in a good state
-      operator bool()const;
+      virtual operator bool()const = 0;
       // returns the number of products needed
-      int qtyNeeded()const;
+      virtual int qtyNeeded()const = 0;
       // retuns the quantity on hand
-      int qty()const;
+      virtual int qty()const = 0;
       // determines if the iProduct is displayed in a linear format or 
       // descriptive format
-      void Linear(bool isLinear);
+      virtual void Linear(bool isLinear) = 0;
       // saves the iProduct into a file
-      std::ofstream& save(std::ofstream& ofstr)const;
+      virtual std::ofstream& save(std::ofstream& ofstr)const = 0;
       // loads an iProduct from a file
-      std::ifstream& load(std::ifstream& ifstr);
+      virtual std::ifstream& load(std::ifstream& ifstr) = 0;
       // displays the iProduct on the screen
-      std::ostream& display(std::ostream& ostr)const;
+      virtual std::ostream& display(std::ostream& ostr)const = 0;
       // reads the iProduct from the console
-      std::istream& read(std::istream& istr);
+      virtual std::istream& read(std::istream& istr) = 0;
       // return true if the sku is a match to the iProduct's sku
-      bool operator==(int sku)const;
+      virtual bool operator==(int sku)const = 0;
       // rtrns true if the description is found in the iPorduct's description
-      bool operator==(const char* description)const;
+      virtual bool operator==(const char* description)const = 0;
 
    };
 }
