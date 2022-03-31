ebf50efc059b0c3d52e382aa86e9a4cdab50d9b3
Francis Colendres <flcolendres@myseneca.ca>
2022-03-31 00:55:58 -0400

    prepared my "wish list" to be continued tomorrow

---
+++ ms4/Perishable.cpp
@@ -0,0 +1,14 @@
+/* ------------------------------------------------------
+Final project Milestone 4
+Module: Perishable
+Filename: Perishable.cpp
+Version 1.0
+Author: Francis Lloyd Colendres  2022-03-31
+Revision History
+-----------------------------------------------------------
+Date          Reason
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
---
+++ ms4/Perishable.h
@@ -0,0 +1,40 @@
+/* ------------------------------------------------------
+Final project Milestone 4
+Module: Perishable
+Filename: Perishable.h
+Version 1.0
+Author: Francis Lloyd Colendres  2022-03-31
+Revision History
+-----------------------------------------------------------
+Date          Reason
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+#ifndef SDDS_PERISHABLE_H
+#define SDDS_PERISHABLE_H
+#include "Item.h"
+#include "Date.h"
+namespace sdds
+{
+   class Perishable : public Item
+   {
+      Date m_expiry;
+      char* m_instruction;
+   public:
+      Perishable();
+      Perishable(const Perishable& P);
+      Perishable& operator=(const Perishable& P);
+      virtual ~Perishable();
+      const Date& expiry();
+      int readSku(std::istream& istr);
+      std::ofstream& save(std::ofstream& ofstr)const;
+      std::ifstream& load(std::ifstream& ifstr);
+      std::ostream& display(std::ostream& ostr)const;
+      std::istream& read(std::istream& istr);
+   };
+}
+
+#endif
