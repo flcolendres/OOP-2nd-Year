1c2f4a8af06a55483f4f140b2f49057c4bbbc082
Francis Colendres <flcolendres@myseneca.ca>
2022-03-31 15:14:21 -0400

    defined some functions

--- ms4/Perishable.cpp
+++ ms4/Perishable.cpp
@@ -12,3 +12,41 @@ Date          Reason
 I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
+#include <iostream>
+#include "Perishable.h"
+#include "Utils.h"
+
+namespace sdds
+{
+   Perishable::Perishable() : Item()
+   {
+      m_expiry = Date();
+      m_instruction = nullptr;
+   }
+
+   Perishable::Perishable(const Perishable& P)
+   {
+      operator=(P);
+   }
+
+   Perishable& Perishable::operator=(const Perishable& P)
+   {
+      if (this != &P)
+      {
+         m_expiry = P.m_expiry;
+         ut.alocpy(m_instruction, P.m_instruction);
+      }
+      return *this;
+   }
+
+   Perishable::~Perishable()
+   {
+      delete[] m_instruction;
+   }
+
+   const Date& Perishable::expiry()
+   {
+      return m_expiry;
+   }
+
+}
