f4d3922c82fe119bc24f13c2de99bd260a404e40
Francis Colendres <flcolendres@myseneca.ca>
2022-04-06 23:14:36 -0400

    adding modules to m56

---
+++ m56/AidMan.h
@@ -0,0 +1,51 @@
+/* ------------------------------------------------------
+Final project Milestone 5
+Module: AidMan
+Filename: AidMan.h
+Version 1.0
+Author: Francis Lloyd Colendres  2022-03-12
+Revision History
+-----------------------------------------------------------
+Date          Reason
+04-06         Changed the way m_iproduct is initialized to safe empty state
+04-06         Functions that call an option are set to private.
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+#ifndef SDDS_AidMan_H
+#define SDDS_AidMan_H
+#include "Menu.h"
+#include "iProduct.h"
+namespace sdds
+{
+   const int sdds_max_num_items = 100;
+   class AidMan
+   {
+      char* m_fileName;
+      Menu m_mainMenu;
+      unsigned int menu() const;
+      iProduct* m_iproduct[sdds_max_num_items]{};
+      int m_numOfIproduct;
+      void listItem();
+      void addItem();
+      void removeItem();
+      void updateItem();
+      void sortItem();
+      void itemDesc(char* input);
+   public:
+      AidMan();
+      AidMan(const AidMan& a) = delete;
+      AidMan& operator=(const AidMan& A) = delete;
+      virtual ~AidMan();
+      void run();
+      void save();
+      void deallocate();
+      void load();
+      void remove(int index);
+      int list(const char* sub_desc = nullptr);
+      int search(int sku) const;
+   };
+}
+#endif
---
+++ m56/Perishable.cpp
@@ -0,0 +1,143 @@
+/* ------------------------------------------------------
+Final project Milestone 5
+Module: Perishable
+Filename: Perishable.cpp
+Version 1.0
+Author: Francis Lloyd Colendres  2022-03-31
+Revision History
+-----------------------------------------------------------
+Date          Reason
+04-05         Cleared istr in read.
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#include <iostream>
+#include <fstream>
+#include "Perishable.h"
+#include "Utils.h"
+using namespace std;
+namespace sdds
+{
+   Perishable::Perishable() : Item()
+   {
+
+   }
+
+   Perishable::Perishable(const Perishable& P)
+   {
+      operator=(P);
+   }
+
+   Perishable& Perishable::operator=(const Perishable& P)
+   {
+      if (this != &P)
+      {
+         Item:: operator=(P);
+         m_expiry = P.m_expiry;
+         ut.alocpy(m_instruction, P.m_instruction);
+         if (m_instruction == nullptr)
+         {
+            m_state = "Empty";
+         }
+      }
+      return *this;
+   }
+
+   Perishable::~Perishable()
+   {
+      delete[] m_instruction;
+   }
+
+   const Date& Perishable::expiry()
+   {
+      return m_expiry;
+   }
+
+   int Perishable::readSku(std::istream& istr)
+   {
+      return ut.getint(10000, 39999, "SKU: ");
+   }
+
+   std::ofstream& Perishable::save(std::ofstream& ofstr) const
+   {
+      Date D;
+      if (*this)
+      {
+         Item::save(ofstr);
+         ofstr << "\t";
+         if (m_instruction && m_instruction[0] != ' ') ofstr << m_instruction;
+         ofstr << "\t";
+         D = m_expiry;
+         D.formatted(false);
+         ofstr << D;
+      }
+      return ofstr;
+   }
+   std::ifstream& Perishable::load(std::ifstream& ifstr)
+   {
+      char temp[1000];
+      Item::load(ifstr);
+      ifstr.getline(temp, 1000, '\t');
+      ut.alocpy(m_instruction, temp);
+      m_expiry.Date::read(ifstr);
+      ifstr.clear();
+      ifstr.ignore(1000, '\n');
+      if (ifstr.fail() || !m_state)
+      {
+         m_state = "Input file stream read (perishable) failed!";
+         ifstr.setstate(ios::badbit);
+      }
+      return ifstr;
+   }
+   std::ostream& Perishable::display(std::ostream& ostr) const
+   {
+      if (!*this)
+         ostr << m_state;
+      else if (linear())
+      {
+         Item::display(ostr);
+         if (m_instruction && m_instruction[0] != '\0')
+            ostr << "*";
+         else
+            ostr << " ";
+         ostr << m_expiry;
+      }
+      else if (!linear())
+      {
+         ostr << "Perishable ";
+         Item::display(ostr);
+         ostr << "Expiry date: ";
+         ostr << m_expiry;
+         if (m_instruction && m_instruction[0] != '\0')
+         {
+            ostr << endl << "Handling Instructions: ";
+            ostr << m_instruction;
+         }
+         ostr << "\n";
+      }
+      return ostr;
+   }
+   std::istream& Perishable::read(std::istream& istr)
+   {
+      char temp[1000];
+      istr.clear();
+      Item::read(istr);
+      delete[] m_instruction;
+      m_instruction = nullptr;
+      cout << "Expiry date (YYMMDD): ";
+      m_expiry.Date::read(istr);
+      istr.ignore(1000, '\n');
+      cout << "Handling Instructions, ENTER to skip: ";
+      if (istr.peek() != '\n')
+      {
+         istr.getline(temp, 1000, '\n');
+         ut.alocpy(m_instruction, temp);
+      }
+      if (!istr)
+      {
+         m_state = "Perishable console date entry failed!";
+      }
+      return istr;
+   }
+}
